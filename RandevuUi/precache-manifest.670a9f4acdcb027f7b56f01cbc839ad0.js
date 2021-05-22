0e722ce66d48b3ffa2d2e49382fa03c2a1d103b8
Ahmet Günaydın <ahmetgunaydin@outlook.com>
2021-05-23 00:23:55 +0300

    aaa

---
+++ RandevuUi/precache-manifest.670a9f4acdcb027f7b56f01cbc839ad0.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2655f648c5fda279ed6",
+    "url": "css/app.b77fdd7f.css"
+  },
+  {
+    "revision": "b460f4710c2dd32415a6",
+    "url": "css/chunk-vendors.c0b9aa44.css"
+  },
+  {
+    "revision": "147e3378b44bc9570418b1eece10dd7c",
+    "url": "fonts/materialdesignicons-webfont.147e3378.woff"
+  },
+  {
+    "revision": "174c02fc4609e8fc4389f5d21f16a296",
+    "url": "fonts/materialdesignicons-webfont.174c02fc.ttf"
+  },
+  {
+    "revision": "64d4cf64afd77a4ad2713f648eb920e6",
+    "url": "fonts/materialdesignicons-webfont.64d4cf64.eot"
+  },
+  {
+    "revision": "7a44ea195f395e1d086010e44555a5c4",
+    "url": "fonts/materialdesignicons-webfont.7a44ea19.woff2"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "0b0daf57aa4a3e496a4d89b297aca147",
+    "url": "index.html"
+  },
+  {
+    "revision": "de192ee71aabff38eeec",
+    "url": "js/about.648367d9.js"
+  },
+  {
+    "revision": "a2655f648c5fda279ed6",
+    "url": "js/app.e299fddb.js"
+  },
+  {
+    "revision": "b460f4710c2dd32415a6",
+    "url": "js/chunk-vendors.f4d1be76.js"
+  },
+  {
+    "revision": "af45f8106e9cf0c348e0cb926688bbef",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
