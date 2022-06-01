a6e4a3229a791b2b6842ad9a78f93d005a05b3dd
Ahmet Günaydın <ahmetgunaydin@outlook.com>
2022-06-02 00:11:52 +0300

    dist msg ok

---
+++ DistMsg/precache-manifest.a9cd64b3ede9214270112344e447515e.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "181b73322abc534c7ed4",
+    "url": "css/app.5111d6e1.css"
+  },
+  {
+    "revision": "3ea87d09283bde076f16",
+    "url": "css/chunk-vendors.c293e063.css"
+  },
+  {
+    "revision": "147e3378b44bc9570418b1eece10dd7c",
+    "url": "fonts/materialdesignicons-webfont.147e3378.woff"
+  },
+  {
+    "revision": "174c02fc4609e8fc4389f5d21f16a296",
+    "url": "fonts/materialdesignicons-webfont.174c02fc.ttf"
+  },
+  {
+    "revision": "64d4cf64afd77a4ad2713f648eb920e6",
+    "url": "fonts/materialdesignicons-webfont.64d4cf64.eot"
+  },
+  {
+    "revision": "7a44ea195f395e1d086010e44555a5c4",
+    "url": "fonts/materialdesignicons-webfont.7a44ea19.woff2"
+  },
+  {
+    "revision": "3620bad7ed690e8e77269b977b2ccccd",
+    "url": "img/Randevu Al - NVI (1).pdf"
+  },
+  {
+    "revision": "9c9fbcb07164999cc77c3fe2a0189617",
+    "url": "img/Randevu Al - NVI (2).pdf"
+  },
+  {
+    "revision": "63127a2b44baa5c60003418f9ed46d61",
+    "url": "img/Randevu Al - NVI (3).pdf"
+  },
+  {
+    "revision": "69be44bde079d297a0746d5097691d93",
+    "url": "img/Randevu Al - NVI (4).pdf"
+  },
+  {
+    "revision": "2727da227a720d07be0181bb09114027",
+    "url": "img/Randevu Al - NVI.pdf"
+  },
+  {
+    "revision": "746f9bbaeea5e00d5c6f61a44a206eac",
+    "url": "img/basvuru_ucretleri_diger.png"
+  },
+  {
+    "revision": "095c0ce66545fc2f1a8e1a94515b44c2",
+    "url": "img/basvurularim.png"
+  },
+  {
+    "revision": "9d70910d7fdb055defeb2d974ecf36f5",
+    "url": "img/basvuruucretleri.png"
+  },
+  {
+    "revision": "1a0f6723714d772032eb2d7c4b6f4a44",
+    "url": "img/dogrulamakodu.png"
+  },
+  {
+    "revision": "a6812fb4c0a28ea991af5f8edc0c19ca",
+    "url": "img/header.png"
+  },
+  {
+    "revision": "cee959bce3c0b53eb1953f820c33ced7",
+    "url": "img/logo-light.svg"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "d1aa58ff793ddf913fb1063a5be0883a",
+    "url": "img/logo.svg"
+  },
+  {
+    "revision": "80811a6bdbb817670af758dfa78c9561",
+    "url": "img/pasaport_talep_formlari.png"
+  },
+  {
+    "revision": "2a8093304a6143229bd1614acfedca1e",
+    "url": "img/randevu_al.png"
+  },
+  {
+    "revision": "34a4f81cc4b0dfe5580e457ef3ce3833",
+    "url": "img/randevuislemleri.png"
+  },
+  {
+    "revision": "aa1fbbdfd8dd6955fb0559b7afd67df2",
+    "url": "img/randevularim.png"
+  },
+  {
+    "revision": "0674f112f69fff3775580bfab968ad4e",
+    "url": "img/randevularim_diger.png"
+  },
+  {
+    "revision": "b9a0055f688d8a4c35ed44979bd1e596",
+    "url": "img/sorular_diger.png"
+  },
+  {
+    "revision": "5205eaaef3f1fe14326b6e1884819eff",
+    "url": "img/sss.png"
+  },
+  {
+    "revision": "9282ff5f9c4ad8b66895638788c0521d",
+    "url": "img/turkuaz.png"
+  },
+  {
+    "revision": "1a3a2b088457611b9d79f66e4fb38a4c",
+    "url": "index.html"
+  },
+  {
+    "revision": "cdd3dc080461df318cbf",
+    "url": "js/about.ba157cf6.js"
+  },
+  {
+    "revision": "181b73322abc534c7ed4",
+    "url": "js/app.41f5b42b.js"
+  },
+  {
+    "revision": "3ea87d09283bde076f16",
+    "url": "js/chunk-vendors.e2fa0f97.js"
+  },
+  {
+    "revision": "c0126075e7d48625c6019d13025b1656",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
---
+++ DistMsg/service-worker.js
@@ -0,0 +1,88 @@
+importScripts("precache-manifest.a9cd64b3ede9214270112344e447515e.js", "workbox-v4.3.1/workbox-sw.js");
+workbox.setConfig({modulePathPrefix: "workbox-v4.3.1"});
+/* eslint-disable prettier/prettier */
+/* eslint-disable no-undef */
+// This is the code piece that GenerateSW mode can't provide for us.
+// This code listens for the user's confirmation to update the app.
+
+
+
+self.addEventListener("message", (e) => {
+console.log("asdwqwdqwdwdw")
+  if (!e.data) {
+    return;
+  }
+  
+
+  switch (e.data) {
+    case "skipWaiting":
+      self.skipWaiting();
+     
+      break;
+
+    default:
+      // NOOP
+      break;
+  }
+});
+
+self.addEventListener('activate', function (event) {
+  event.waitUntil(
+    caches.keys().then(function (cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function (cacheName) {
+
+          console.log(cacheName, "siliniyor sw...")
+          return true;
+        }).map(function (cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+ 
+
+
+});
+self.addEventListener('notificationclick', event => {
+  console.log("click")
+  event.waitUntil(self.clients.openWindow('/#/siraal'));
+});
+
+
+self.addEventListener('push', function(event) {
+
+  console.log('[Service Worker] Push Received.', event.data.json());
+  // console.log(`[Service Worker] Push had this data: `,event.data.json());
+  //var data= event.data.json();
+
+  const timestamp = new Date().getTime() + 10 * 1000;
+  //const title = data.Title;
+
+
+
+  const options = {
+    body: "sdfsfsfsdf",
+    //showTrigger: new TimestampTrigger(timestamp),
+    icon: "./img/icons/android-chrome-512x512.png",
+    vibrate: [200, 100, 200, 100, 200, 100, 200],
+    
+    //timestamp:new Date().getTime() + 120 * 1000
+  };
+ 
+ // event.waitUntil(
+    //self.registration.showNotification("title", options)
+  //);
+
+  
+  
+});
+
+workbox.core.clientsClaim(); // Vue CLI 4 and Workbox v4, else
+// workbox.clientsClaim(); // Vue CLI 3 and Workbox v3.
+
+// The precaching code provided by Workbox.
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+// workbox.precaching.suppressWarnings(); // Only used with Vue CLI 3 and Workbox v3.
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
